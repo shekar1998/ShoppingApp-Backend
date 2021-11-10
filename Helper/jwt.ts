d0ee6767f1f83decd102fc72d22913416b485e48
your_username <email>
2021-11-10 18:44:43 +0530

    v4

--- Helper/jwt.ts
+++ Helper/jwt.ts
@@ -14,7 +14,7 @@ export function ExpressJwt() {
       { url: /\/public\/Image(.*)/, methods: ['GET', 'OPTIONS'] },
       { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
       { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-      { url: /\/api\/v1\/users(.*)/, methods: ['GET', 'PUT', 'OPTIONS'] },
+      { url: /\/api\/v1\/users(.*)/, methods: ['PUT', 'OPTIONS'] },
       `${api}/users/login`,
       `${api}/users/register`,
     ],
